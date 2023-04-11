4cb04b938b940ea2bd72b9c2d3e2fc2b7ad34827
Alumno <nodaxdx@gmail.com>
2023-04-10 22:12:42 -0500

    Primer commit

---
+++ WPFSemana4/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFSemana4
+{
+    public class Person
+    {
+        public string PersonID { get; set; }
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+    }
+}
